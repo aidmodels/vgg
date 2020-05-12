9197e2aaef224792827d2f13f9a5f6c271caae7b
cyy <sheeppooo.cy@gmail.com>
2020-05-13 01:21:17 +0200

    add vgg

---
+++ README.md
@@ -0,0 +1,10 @@
+# aidmodels/vgg
+
+> vgg
+
+This project is based on the AID Project. Common topics are discussed in the [AID docs](https://aid.autoai.org). Make sure to read it!
+
+## Usage
+
+
+## Reference
---
+++ vgg/solver.py
@@ -0,0 +1,44 @@
+from aid.solver import Solver
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+
+class ImageClassificationSolver(Solver):
+    def __init__(self, toml_file=None):
+        super().__init__(toml_file)
+        # Do you Init Work here
+        self.model = VGG16(weights="./pretrained/vgg16_weights_tf_dim_ordering_tf_kernels.h5")
+
+        self.ready()
+    def infer(self, data):
+        # if you need to get file uploaded, get the path from input_file_path in data
+        # load an image from file
+        image = load_img(data['input_file_path'], target_size=(224, 224))
+        
+        # convert the image pixels to a numpy array
+        image = img_to_array(image)
+
+        # reshape data for the model
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+
+        # prepare the image for the VGG model
+        image = preprocess_input(image)
+
+        # predict the probability across all output classes
+        yhat = self.model.predict(image)
+
+        # convert the probabilities to class labels
+        label = decode_predictions(yhat)
+        # retrieve the most likely result, e.g. highest probability
+        label = label[0][0]
+        # print the classification
+        print('%s (%.2f%%)' % (label[1], label[2]*100))
+
+        result = {
+            "label": label[1],
+            "confidence": label[2]*100
+        }
+
+        return result # return a dict
